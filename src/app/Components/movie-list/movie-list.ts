a19504e13bee4ac214a41f7d9b6d604a7cf8ef92
Alin <alinsridhar1@gmail.com>
2026-01-10 23:13:44 +0530

    added more and previous buttons for navigation on movies page and added some styles

--- src/app/Components/movie-list/movie-list.ts
+++ src/app/Components/movie-list/movie-list.ts
@@ -11,18 +11,43 @@ import { Router } from '@angular/router';
 export class MovieList implements OnInit{
   constructor(private apiService: Apiservices,private cdr: ChangeDetectorRef,private route:Router) {}
   movies:any[] =[];
-
+  i:number = 2;
   ngOnInit(): void {
     this.apiService.getTopRatedMovies(1).subscribe((data:any) => {
       this.movies = data.results;
-    });
-
-    this.apiService.getTopRatedMovies(2).subscribe((data:any) => {
-      this.movies = this.movies.concat(data.results);
       this.cdr.detectChanges();
     });
     
   }
+  nextpage(){
+    this.apiService.getTopRatedMovies(this.i).subscribe((data:any) => {
+      if(data.results.length==0){
+        alert("No more pages available");
+        return;
+      }
+      console.log("before next",this.i);
+      this.movies = data.results;
+      this.i++;
+      console.log("after next",this.i);
+      this.cdr.detectChanges();
+    });
+  }
+  previouspage(){
+    console.log("inside previous",this.i);
+    if(this.i>2){
+      console.log("before previous",this.i);
+    this.i = this.i-2;
+    this.apiService.getTopRatedMovies(this.i).subscribe((data:any) => {
+      this.movies = data.results;
+      console.log("after previous",this.i);
+      this.i++;
+      this.cdr.detectChanges();
+    });
+  }
+  else{
+    alert("There are no previous pages");
+  }
+}
   viewmoviedetails(movie:any){
     this.route.navigate(['/movies',movie.id],{state:{movie}});
   }
