376c215729f6f7b3083371f82d6d052718e3db00
Alin <alinsridhar1@gmail.com>
2026-01-10 22:28:40 +0530

    added routes

--- src/app/Components/navs/navs.ts
+++ src/app/Components/navs/navs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navs',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navs.html',
   styleUrl: './navs.css',
 })
